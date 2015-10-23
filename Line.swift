1bf3dc797b10b6a6450cbd56c5e449167492fed3
matthew-compton <compton@bignerdranch.com>
2015-10-23 11:46:27 -0400

    Finished Chapter 18

---
+++ Line.swift
@@ -0,0 +1,17 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Matthew Compton on 10/23/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+    
+}
